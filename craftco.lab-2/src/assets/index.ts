2febce1126069da28b093c7f4daca76156fbb756
Gtm0215 <gautamlal0215@gmail.com>
2026-03-13 01:41:23 +0530

    Create index.ts

---
+++ craftco.lab-2/src/assets/index.ts
@@ -0,0 +1,23 @@
+import paintings from "./paintings.png"
+import customPaintings from "./custom-paintings.png"
+import bookmarks from "./bookmarks.png"
+import postcards from "./postcards.png"
+import customBulbDeco from "./custom-bulb-deco.png"
+import customWallCd from "./custom-wall-cd.png"
+import oceanVinyl from "./ocean-vinyl.png"
+import xmasPostcard from "./xmas-postcard.png"
+import animalCanvas from "./animal-canvas.png"
+import giftHamper from "./gift-hamper.png"
+
+export {
+paintings,
+customPaintings,
+bookmarks,
+postcards,
+customBulbDeco,
+customWallCd,
+oceanVinyl,
+xmasPostcard,
+animalCanvas,
+giftHamper
+}
