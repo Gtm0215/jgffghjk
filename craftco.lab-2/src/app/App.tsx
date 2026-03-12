a4ca04ccba5f59a29bf92dd3c8eb82d4fa240000
Gtm0215 <gautamlal0215@gmail.com>
2026-03-13 01:47:34 +0530

    Update App.tsx

--- craftco.lab-2/src/app/App.tsx
+++ craftco.lab-2/src/app/App.tsx
@@ -88,3 +88,57 @@ return ( <div>
 
 )
 }
+    <div className="card">
+      <img src={postcards} />
+      <p>Postcards</p>
+    </div>
+
+  </div>
+
+  <h2>Featured Products</h2>
+
+  <div className="products">
+
+    <div className="product">
+      <img src={customBulbDeco}/>
+      <h3>Custom Bulb Deco</h3>
+      <p>₹299</p>
+    </div>
+
+    <div className="product">
+      <img src={customWallCd}/>
+      <h3>Custom Wall CD Hanging</h3>
+      <p>₹299</p>
+    </div>
+
+    <div className="product">
+      <img src={oceanVinyl}/>
+      <h3>Ocean on Vinyl</h3>
+      <p>₹499</p>
+    </div>
+
+    <div className="product">
+      <img src={xmasPostcard}/>
+      <h3>Custom Xmas Postcard</h3>
+      <p>₹150</p>
+    </div>
+
+    <div className="product">
+      <img src={animalCanvas}/>
+      <h3>Animal Canvas</h3>
+      <p>₹599</p>
+    </div>
+
+    <div className="product">
+      <img src={giftHamper}/>
+      <h3>Mystery Craft Hamper</h3>
+      <p>₹1299</p>
+    </div>
+
+  </div>
+
+</div>
+```
+
+)
+}
